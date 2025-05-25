d2025888ce47e0f8076150a4a6752a6ffa048126
DEEPAK SAGAR <sagardeepak2002@gmail.com>
2025-05-25 15:27:36 +0530

    Update MemcachedConfig.java

--- src/main/java/com/example/devopsapp/config/MemcachedConfig.java
+++ src/main/java/com/example/devopsapp/config/MemcachedConfig.java
@@ -2,6 +2,7 @@ package com.example.devopsapp.config;
 
 import net.rubyeye.xmemcached.XMemcachedClient;
 import net.rubyeye.xmemcached.MemcachedClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.support.SimpleCacheManager;
@@ -14,9 +15,17 @@ import java.io.IOException;
 @EnableCaching
 public class MemcachedConfig {
 
+    // Inject memcached host and port from application.properties
+    @Value("${memcached.host}")
+    private String memcachedHost;
+
+    @Value("${memcached.port}")
+    private int memcachedPort;
+
     @Bean
     public MemcachedClient memcachedClient() throws IOException {
-        return new XMemcachedClient("memcached", 11211);
+        // Use injected host and port instead of hardcoded values
+        return new XMemcachedClient(memcachedHost, memcachedPort);
     }
 
     @Bean
